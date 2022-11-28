2521995fc6d14ff9f384294e43db5b4c05b7caa7
Boris Chepurnoy <beastbs.dev@gmail.com>
2022-11-28 11:56:27 +0200

    replaced createStore by configureStore

--- src/store/index.js
+++ src/store/index.js
@@ -1,60 +1,26 @@
-/* eslint-disable no-unused-vars */
-
-import { createStore, combineReducers, compose, applyMiddleware } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
-import ReduxThunk from "redux-thunk";
 
 import heroesReducer from "../reducers/heroes";
 import filtersReducer from "../reducers/filters";
 
-const stringMiddleware = (store) => (next) => (action) => {
-  // { dispatch, getState } = (store)
-  if (typeof action === "string") {
-    console.warn("STATE FROM MIDDLEWARE: ", store.getState());
-    return next({ type: action });
-  }
+const stringMiddleware =
+  ({ getState }) =>
+  (next) =>
+  (action) => {
+    if (typeof action === "string") {
+      console.warn("STATE FROM MIDDLEWARE: ", getState());
+      return next({ type: action });
+    }
 
-  return next(action);
-};
-
-const enhancer =
-  (createStore) =>
-  (...args) => {
-    const store = createStore(...args);
-    const originDispatch = store.dispatch;
-
-    console.warn("STATE FROM ENHANCER: ", store.getState());
-
-    store.dispatch = (action) => {
-      if (typeof action === "string") {
-        return originDispatch({
-          type: action,
-        });
-      }
-      return originDispatch(action);
-    };
-
-    return store;
+    return next(action);
   };
 
-const rootReducer = combineReducers({
-  heroesReducer,
-  filtersReducer,
+const store = configureStore({
+  reducer: { heroesReducer, filtersReducer },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(stringMiddleware, logger),
+  devTools: process.env.NODE_ENV !== "production",
 });
-
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(ReduxThunk, stringMiddleware, logger),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-
-  // applyMiddleware(stringMiddleware, logger),
-
-  // compose(
-  //   enhancer,
-  //   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  // )
-);
 
 export default store;
